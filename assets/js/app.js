bb05fe738b1cf70f3cb8d509c50723e5ee780539
Fahad Billah <fahadbillah@yahoo.com>
2016-11-27 01:29:25 +0600

    frontend login done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
 var FLYERBD = angular.module('FLYERBD', [
 	'ngRoute',
 	'oc.lazyLoad',
+	'ngFacebook'
 	]);
 
+FLYERBD.constant('FACEBOOK_APP_ID', '326474321078728');
 
-FLYERBD.config(['$routeProvider', function ($routeProvider) {
+FLYERBD.config(['$routeProvider','$facebookProvider','FACEBOOK_APP_ID', function ($routeProvider,$facebookProvider, facebookAppId) {
+
+	$facebookProvider.setAppId(facebookAppId);
+	$facebookProvider.setPermissions("email,user_friends");
 
 	$routeProvider
 	.when('/', {
@@ -52,3 +57,17 @@ FLYERBD.config(['$routeProvider', function ($routeProvider) {
 		}
 	})
 }]);
+
+FLYERBD.run([function() {
+  (function(){
+     if (document.getElementById('facebook-jssdk')) {return;}
+     var firstScriptElement = document.getElementsByTagName('script')[0];
+     var facebookJS = document.createElement('script'); 
+     facebookJS.id = 'facebook-jssdk';
+     facebookJS.src = '//connect.facebook.net/en_US/all.js';
+     firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+   }());
+}]);
+
+
+
